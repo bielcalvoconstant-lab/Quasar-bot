2e11b817283f16882930a3f87b9de4f6e5fe8a7c
bielcalvoconstant-lab <bielcalvo.constant@gmail.com>
2026-06-24 18:05:10 -0300

    Add files via upload

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,1 +1,24 @@
+module.exports = {
+  name: 'interactionCreate',
+  async execute(interaction) {
+    if (!interaction.isChatInputCommand()) return;
 
+    const command = interaction.client.commands.get(interaction.commandName);
+
+    if (!command) return;
+
+    try {
+      await command.execute(interaction);
+    } catch (error) {
+      console.error(`[ERRO EXECUÇÃO COMANDO] erro ao executar o comando ${interaction.commandName}:`, error);
+      
+      const errorMessage = { content: 'Houve um erro interno ao tentar processar este comando.', ephemeral: true };
+      
+      if (interaction.replied || interaction.deferred) {
+        await interaction.followUp(errorMessage);
+      } else {
+        await interaction.reply(errorMessage);
+      }
+    }
+  }
+};
